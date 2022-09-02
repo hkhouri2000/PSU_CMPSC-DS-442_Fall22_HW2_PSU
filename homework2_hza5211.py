e28a12821f4a731004b2cd21031e2a2f798a8758
hkhouri2000 <hkhouri2000@gmail.com>
2022-09-02 17:27:00 -0400

    Update homework2_hza5211.py

--- homework2_hza5211.py
+++ homework2_hza5211.py
@@ -8,7 +8,10 @@ student_name = "Type your full name here."
 # Imports
 ############################################################
 
-# Include your imports here, if any are used.
+from math import factorial
+from itertools import permutations
+from random import random 
+# Usable imports: collections, itertools, math, random 
 
 
 
@@ -17,16 +20,38 @@ student_name = "Type your full name here."
 ############################################################
 
 def num_placements_all(n):
-    pass
+    # We are taking n placements on a grid with (n x n) possible positions 
+    return factorial(n * n) // factorial(n) // factorial(n * n - n)
 
 def num_placements_one_per_row(n):
-    pass
+    # For each row, a queen can be placed in n! ways. We multiply with the
+    # number of rows to get the total number of placements. 
+    return n * factorial(n)
 
 def n_queens_valid(board):
-    pass
+    # Check all possible pairs of queens
+    for i in range(len(board)):
+        for j in range(len(board)):
+            # Do not compare queen to itself
+            if i == j:
+                continue 
+
+            # Check if two queens are at the same column
+            if board[i] == board[j]:
+                return False 
+            
+            # If the difference in row is the same as the difference in column,
+            # then the two queens must be on the same diagonal 
+            if abs(i - j) == abs(board[i] - board[j]):
+                return False
+
+    return True 
 
 def n_queens_solutions(n):
-    pass
+    # Create all permutations and test each if it is valid 
+    for comb in permutations(range(n), n):
+        if n_queens_valid(comb):
+            yield list(comb)
 
 ############################################################
 # Section 2: Lights Out
